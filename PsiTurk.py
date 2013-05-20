d6edfcd9e9730c17f9e11734f5610b509157436b
Jay B. Martin <jmartin805@gmail.com>
2013-05-20 17:36:52 -0400

    hook and lanuch dashboard

--- PsiTurk.py
+++ PsiTurk.py
@@ -3,10 +3,18 @@
 
 from psiturk_server import PsiTurkServer
 import multiprocessing
+import subprocess
 from config import config
 import webbrowser
 
+DASHBOARD_PORT = "5000"  # TODO(Todd, Jay): Change this to user defined param via app
+
+# Launch dashboard kernel
+subprocess.Popen("python dashboard_server.py", shell=True,
+                 stdout=subprocess.PIPE,
+                 stderr=subprocess.STDOUT)
 workers = multiprocessing.cpu_count() * 2 + 1
+
 if __name__ == "__main__":
     loglevels = ["debug", "info", "warning", "error", "critical"]
     options = {
@@ -16,8 +24,7 @@ if __name__ == "__main__":
         'loglevel': loglevels[config.getint("Server Parameters", "loglevel")]
     }
 
-
-launchurl = "http://"+config.get("Server Parameters", "host")+":"+str(config.getint('Server Parameters', 'port'))+"/dashboard"
+launchurl = "http://"+config.get("Server Parameters", "host")+DASHBOARD_PORT+"/dashboard"
 webbrowser.open(launchurl, new=1, autoraise=True)
 
 PsiTurkServer(options).run()
