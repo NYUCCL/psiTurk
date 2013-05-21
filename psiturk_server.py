4e1115f0f24e649bb08305b60fea106da6506392
Jay B. Martin <jmartin805@gmail.com>
2013-05-21 16:17:30 -0400

    Now self contained and callable as a subprocess.

--- psiturk_server.py
+++ psiturk_server.py
@@ -3,26 +3,28 @@ from gunicorn.app.base import Application
 from gunicorn import util
 import sys
 import os
+import multiprocessing
+from config import config
+
 
 class PsiTurkServer(Application):
     '''
     Custom Gunicorn Server Application
     '''
-    
-    def __init__(self, options={}):
+
+    def __init__(self):
         '''__init__ method
-        
         Load the base config and assign some core attributes.
         '''
+        self.load_user_config()
         self.usage = None
         self.callable = None
-        self.options = options
+        self.options = self.user_options
         self.prog = None
         self.do_load_config()
-    
+
     def init(self, *args):
         '''init method
-        
         Takes our custom options from self.options and creates a config
         dict which specifies custom settings.
         '''
@@ -31,11 +33,22 @@ class PsiTurkServer(Application):
             if k.lower() in self.cfg.settings and v is not None:
                 cfg[k.lower()] = v
         return cfg
-    
+
     def load(self):
         '''load method
-        
         Imports our application and returns it to be run.
-        '''        
-        print sys.path
+        '''
         return util.import_app("app:app")
+
+    def load_user_config(self):
+        self.loglevels = ["debug", "info", "warning", "error", "critical"]
+        self.user_options = {
+            'bind': config.get("Server Parameters", "host") + ":" + config.get("Server Parameters", "port"),
+            'workers': str(multiprocessing.cpu_count() * 2 + 1),
+            'loglevels': self.loglevels,
+            'loglevel': self.loglevels[config.getint("Server Parameters", "loglevel")]
+        }
+
+
+if __name__ == "__main__":
+    PsiTurkServer().run()
