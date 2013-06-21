69177ca063afd07e52d2524354fb8e28a2fdc0ff
Jay B. Martin <jmartin805@gmail.com>
2013-06-21 11:38:56 -0400

    moved server routines to dashboard

--- app.py
+++ app.py
@@ -89,25 +89,6 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
-
-#----------------------------------------------
-# Server routines
-#----------------------------------------------
-def shutdown_server():
-    # This assumes that psiTurk is the only gunicorn process. Otherwise, problems...
-    gunicorn_pid = find_process("gunicorn: master")
-    os.kill(gunicorn_pid, 15)
-
-def find_process(name):
-    ps = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE, close_fds=True)
-    grep = subprocess.Popen(['grep', str(name)], stdin=ps.stdout, stdout=subprocess.PIPE, close_fds=True)
-    remove_grep = subprocess.Popen(['grep', '-v','grep'], stdin=grep.stdout, stdout=subprocess.PIPE, close_fds=True)
-    awk = subprocess.Popen(['awk', '{print $2}'], stdin=remove_grep.stdout,\
-                           stdout=subprocess.PIPE, close_fds=True).communicate()[0]
-    return int(awk)
-
-
-
 #----------------------------------------------
 # ExperimentError Exception, for db errors, etc.
 #----------------------------------------------
@@ -525,20 +506,6 @@ def dumpdata():
     response.headers['Content-Disposition'] = 'attachment;filename=data.csv'
     response.headers['Content-Type'] = 'text/csv'
     return response
-
-
-#----------------------------------------------
-# psiTurk server routes
-#----------------------------------------------
-@app.route('/shutdown', methods=["GET"])
-def shutdown():
-    external_hash = request.args['hash']
-    if external_hash == HASH:
-        print 'Server shutting down...'
-        shutdown_server()
-        return 'Server shutting down...'
-    else:
-        return 'Unauthorized'
 
 
 #----------------------------------------------
