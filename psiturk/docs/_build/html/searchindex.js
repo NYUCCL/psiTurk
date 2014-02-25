4d54748632a13f2fa8a2f6f91d504808f2d3825b
David Halpern <djh2142@columbia.edu>
2014-02-25 16:38:49 -0500

    Docs
    Built some sample doc pages

---
+++ psiturk/docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["psiTurk Documentation"],objects:{},titleterms:{document:0,tabl:0,welcom:[],indic:0,psiturk:0}})
