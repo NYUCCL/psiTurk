cdb0dee3c9575f937c310255ce41501edcfab1ee
Anna Coenen <coenen.anna@gmail.com>
2014-05-07 12:43:42 -0400

    fix bonus status code (should be 7) and counterbalancing criteria

--- psiturk/experiment.py
+++ psiturk/experiment.py
@@ -135,6 +135,8 @@ def get_random_condcount():
                    filter(Participant.codeversion == config.get('Task Parameters', 'experiment_code_version')).\
                    filter(or_(Participant.status == COMPLETED,
                               Participant.status == CREDITED,
+                              Participant.status == SUMBITTED,
+                              Participant.status == BONUSED,
                               Participant.beginhit > starttime)).\
                    all()
     counts = Counter()
