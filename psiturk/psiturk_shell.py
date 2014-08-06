c9e8bb321be8e153951f2bb9f75b5cd21bd16e0b
JSlote <joseph.slote@gmail.com>
2014-08-05 21:08:11 -0500

    add notice

--- psiturk/psiturk_shell.py
+++ psiturk/psiturk_shell.py
@@ -561,7 +561,8 @@ class PsiturkNetworkShell(PsiturkShell):
         ''' Overloads intro prompt with network-aware version if you can reach
         psiTurk.org, request system status message'''
         server_msg = self.web_services.get_system_status()
-        return server_msg + colorize('psiTurk version ' + version_number +
+        thisfork = "\n----------------------------\n-< You\'re on the JSlote Fork\n----------------------------\n"
+        return server_msg + thisfork + colorize('psiTurk version ' + version_number +
                                      '\nType "help" for more information.',
                                      'green', False)
 
