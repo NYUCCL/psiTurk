5882bc4f8f9305cf59ae7afce18ca953c3f7b370
Esteban Buz <ebuz@bcs.rochester.edu>
2014-08-16 21:24:36 -0400

    Removed the length specification for the datastring column in the participant model. Database creation fails for postgreSQL when a length is specified for TEXT type columns.

--- psiturk/models.py
+++ psiturk/models.py
@@ -34,7 +34,7 @@ class Participant(Base):
     endhit = Column(DateTime)
     bonus = Column(Float, default = 0)
     status = Column(Integer, default = 1)
-    datastring = Column(Text(4294967295))
+    datastring = Column(Text)
     
     def __init__(self, **kwargs):
         self.uniqueid = "{workerid}:{assignmentid}".format(**kwargs)
