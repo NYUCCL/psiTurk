d1e7446f6f2de40095b34d21216acb936f10d68f
Todd Gureckis <todd.gureckis@nyu.edu>
2014-08-19 13:15:39 -0400

    ignore case on config option, what about if database table or username contains 'postgres'

--- psiturk/models.py
+++ psiturk/models.py
@@ -34,7 +34,7 @@ class Participant(Base):
     endhit = Column(DateTime)
     bonus = Column(Float, default = 0)
     status = Column(Integer, default = 1)
-    if 'postgres' in config.get('Database Parameters', 'database_url'):
+    if 'postgres://' in config.get('Database Parameters', 'database_url').lower():
         datastring = Column(Text)
     else:
         datastring = Column(Text(4294967295))
