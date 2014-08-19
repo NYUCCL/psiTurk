15b6316e8d4b8ffc649eed2149733fd3826efe07
Todd Gureckis <todd.gureckis@nyu.edu>
2014-08-19 13:29:57 -0400

    checks for mysql-python library before starting up SQLAlchemy to avoid unfriend crash (Closes #97)

--- psiturk/db.py
+++ psiturk/db.py
@@ -2,13 +2,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
-
 from psiturk_config import PsiturkConfig
 
 config = PsiturkConfig()
 config.load_config()
 
 DATABASE = config.get('Database Parameters', 'database_url')
+
+if 'mysql' in config.get('Database Parameters', 'database_url').lower():
+	try:
+		 __import__('imp').find_module('MySQLdb')
+	except ImportError:
+		print("Sorry, to use a MySQL database you need to install "
+			  "the `mysql-python` python package.  Try `pip mysql-python`. "
+			  "Hopefully it goes smoothly for you.  Installation can "
+			  "be tricky on some systems.")
+		exit()
 
 engine = create_engine(DATABASE, echo=False) 
 db_session = scoped_session(sessionmaker(autocommit=False,
