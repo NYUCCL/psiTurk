bf859968d3db94a6f49a8d62910674433d82fb8c
Dave Eargle <dave@daveeargle.com>
2019-06-21 13:51:49 -0600

    add pytest-mock to test-requirements

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
             ]
         },
         setup_requires = ['pytest-runner'],
-        tests_require=['pytest','ciso8601'],
+        tests_require=['pytest','ciso8601','pytest-mock'],
         author = "NYU Computation and Cognition Lab",
         author_email = "authors@psiturk.org",
         description = "An open platform for science on Amazon Mechanical Turk",
