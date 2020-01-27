5ddbc98ca0206a6b602988c28eccd5b9d79372d6
David <david.halpern@nyu.edu>
2020-01-27 11:33:20 -0500

    add import

--- psiturk/psiturk_config.py
+++ psiturk/psiturk_config.py
@@ -8,6 +8,7 @@ import os
 import sys
 from distutils import file_util
 import six
+from .experiment_errors import EphemeralContainerDBError
 if six.PY2:
     from ConfigParser import ConfigParser
 else:
