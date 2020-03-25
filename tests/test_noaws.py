35aa194febb9c279b18a2e5385587fd2dd2f8226
Dave Eargle <dave@daveeargle.com>
2020-03-25 16:19:36 -0600

    fix noaws tests

--- tests/test_noaws.py
+++ tests/test_noaws.py
@@ -11,7 +11,7 @@ def psiturk_shell(mocker):
     import psiturk.psiturk_shell
     mocker.patch.object(psiturk.psiturk_shell.PsiturkNetworkShell, 'get_intro_prompt', lambda *args, **kwargs: '')
 
-def test_awskeys_invalid(capfd, experiment_server_controller, psiturk_shell):
+def test_awskeys_invalid_shell(capfd, experiment_server_controller, psiturk_shell):
     import psiturk.experiment_server_controller
     from psiturk.psiturk_config import PsiturkConfig
     from psiturk.psiturk_shell import PsiturkNetworkShell
@@ -36,10 +36,10 @@ def test_awskeys_invalid(capfd, experiment_server_controller, psiturk_shell):
         pass
     
     out, err = capfd.readouterr()
-    assert NoMturkConnectionError().message in out
+    assert NoMturkConnectionError().message in err
     
     
-def test_awscreds_notset(capfd, experiment_server_controller, psiturk_shell):
+def test_awscreds_notset_shell(capfd, experiment_server_controller, psiturk_shell):
     from psiturk.psiturk_config import PsiturkConfig
     from psiturk.psiturk_shell import PsiturkNetworkShell
     import psiturk.psiturk_shell as ps
@@ -64,4 +64,17 @@ def test_awscreds_notset(capfd, experiment_server_controller, psiturk_shell):
         pass
     
     out, err = capfd.readouterr()
-    assert AWSAccessKeysNotSetError().message in out
+    assert AWSAccessKeysNotSetError().message in err
+    
+def test_nonaws_still_can_do(capfd, edit_config_file, experiment_server_controller, psiturk_shell):
+    import os
+    from psiturk.psiturk_shell import run
+    edit_config_file('persistent_history_file = .psiturk_history', 'persistent_history_file = ')
+    edit_config_file('launch_in_sandbox_mode = false', 'launch_in_sandbox_mode = true')
+    os.environ['AWS_ACCESS_KEY_ID'] = ''
+    os.environ['AWS_SECRET_ACCESS_KEY'] = ''
+    
+    run(execute='version', quiet=True)
+    out, err = capfd.readouterr()
+    assert 'version' in out
+    
