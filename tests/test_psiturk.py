06cae6a6fffc159d1382b711a6ba6fb522373e92
Dave Eargle <dave@daveeargle.com>
2020-10-08 01:07:22 -0600

    have to account for the `;` at the beginning now

--- tests/test_psiturk.py
+++ tests/test_psiturk.py
@@ -119,13 +119,11 @@ def test_custom_get_condition_not_necessary(tmpdir, mocker, psiturk_test_client)
     assert get_condition('') == (0,0)
 
 def test_missing_template_exception(edit_config_file, remove_template, psiturk_test_client):
-    edit_config_file('use_psiturk_ad_server = true', 'use_psiturk_ad_server = false')
     remove_template('closepopup.html')
     with pytest.raises(RuntimeError):
         app = psiturk_test_client()
 
 def test_notmissing_template(edit_config_file, remove_template, psiturk_test_client):
-    edit_config_file('use_psiturk_ad_server = true', 'use_psiturk_ad_server = false')
     psiturk_test_client()
 
 def test_does_not_die_if_no_custompy(remove_file, psiturk_test_client):
