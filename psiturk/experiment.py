79f16bdf1f2464d064d61fc7a284b4848a843809
Dave Eargle <dave@daveeargle.com>
2021-03-27 15:11:56 -0600

    defer all logging to gunicorn if gunicorn is in use

--- psiturk/experiment.py
+++ psiturk/experiment.py
@@ -33,26 +33,6 @@ from .user_utils import nocache
 CONFIG = PsiturkConfig()
 CONFIG.load_config()
 
-# Setup logging
-# taken from: https://stackoverflow.com/a/44760039/2714651
-handlers = []
-if 'ON_CLOUD' in os.environ:
-    stream_handler = logging.StreamHandler(sys.stderr)
-    handlers += [stream_handler]
-else:
-    file_path = os.path.join(os.getcwd(), CONFIG.get("Server Parameters", "logfile"))
-    file_handler = logging.FileHandler(filename=file_path)
-    handlers += [file_handler]
-
-LOG_LEVELS = [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR,
-              logging.CRITICAL]
-LOG_LEVEL = LOG_LEVELS[CONFIG.getint('Server Parameters', 'loglevel')]
-logging.basicConfig(handlers=handlers, format='%(asctime)s %(message)s',
-                    level=LOG_LEVEL)
-
-
-# Status codes
-
 # Let's start
 # ===========
 
@@ -60,8 +40,23 @@ app = Flask("Experiment_Server")
 
 # experiment server logging
 if 'gunicorn' in os.environ.get('SERVER_SOFTWARE', ''):
-    gunicorn_error_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers.extend(gunicorn_error_logger.handlers)
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+else:
+    errorlog = CONFIG.get("Server Parameters", "errorlog")
+    if errorlog == '-':
+        handler = logging.StreamHandler(sys.stderr)
+    else:
+        file_path = os.path.join(os.getcwd(), errorlog)
+        handler = logging.FileHandler(filename=file_path)
+
+    LOG_LEVELS = [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR,
+                  logging.CRITICAL]
+    LOG_LEVEL = LOG_LEVELS[CONFIG.getint('Server Parameters', 'loglevel')]
+
+    logging.basicConfig(handlers=[handler], format='%(asctime)s %(message)s',
+                        level=LOG_LEVEL)
 
 # Set cache timeout to 10 seconds for static files
 app.config.update(SEND_FILE_MAX_AGE_DEFAULT=10)
