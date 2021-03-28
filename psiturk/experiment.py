990db90dc34a36012c1edf88b3172c62b145179c
Dave Eargle <dave@daveeargle.com>
2021-03-27 19:34:18 -0600

    undo defer to gunicorn
    only set filename logger if not logging to `-` (flask defaults to 
    logging to stdout)

--- psiturk/experiment.py
+++ psiturk/experiment.py
@@ -33,30 +33,21 @@ from .user_utils import nocache
 CONFIG = PsiturkConfig()
 CONFIG.load_config()
 
+LOG_LEVELS = [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR,
+logging.CRITICAL]
+LOG_LEVEL = LOG_LEVELS[CONFIG.getint('Server Parameters', 'loglevel')]
+
+logfile = CONFIG.get("Server Parameters", "errorlog")
+if logfile != '-':
+    file_path = os.path.join(os.getcwd(), logfile)
+    logging.basicConfig(filename=file_path, format='%(asctime)s %(message)s',
+                        level=LOG_LEVEL)
+
 # Let's start
 # ===========
 
 app = Flask("Experiment_Server")
-
-# experiment server logging
-if 'gunicorn' in os.environ.get('SERVER_SOFTWARE', ''):
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
-else:
-    errorlog = CONFIG.get("Server Parameters", "errorlog")
-    if errorlog == '-':
-        handler = logging.StreamHandler(sys.stderr)
-    else:
-        file_path = os.path.join(os.getcwd(), errorlog)
-        handler = logging.FileHandler(filename=file_path)
-
-    LOG_LEVELS = [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR,
-                  logging.CRITICAL]
-    LOG_LEVEL = LOG_LEVELS[CONFIG.getint('Server Parameters', 'loglevel')]
-
-    logging.basicConfig(handlers=[handler], format='%(asctime)s %(message)s',
-                        level=LOG_LEVEL)
+app.logger.setLevel(LOG_LEVEL)
 
 # Set cache timeout to 10 seconds for static files
 app.config.update(SEND_FILE_MAX_AGE_DEFAULT=10)
